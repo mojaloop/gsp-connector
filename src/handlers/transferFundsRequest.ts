bd4c286dbd73b82ec556a5c7545d76ed5391557b
Miguel de Barros <miguel@debarros.me>
2022-09-29 18:30:53 +0200

    added transferFundsRequest and fixed transactionRequestId correlation
    - added imp for transferFundsRequest
    - fixed transactionRequestId correlation issue between initiate and approve steps

--- src/handlers/transferFundsRequest.ts
+++ src/handlers/transferFundsRequest.ts
@@ -5,58 +5,77 @@ import { StateResponseToolkit } from '../server/plugins/state'
 // import { thirdpartySdk, gsp } from '@project-interfaces';
 import { thirdpartySdk, gsp } from '../interface/types';
 
+import config from '../shared/config'
+
 async function post(_context: unknown, _request: Request, h: StateResponseToolkit): Promise<ResponseObject>  {
-  const transferFundsRequest = _request.payload //as gsp.components['schemas']['transferFundsRequest'];
+  const transferFundsRequest = _request.payload as gsp.components["schemas"]["TransferFundsRequest"]
   console.log('transferFundsRequest:')
   console.dir(transferFundsRequest);
 
-  // TODO: Map transferFundsRequest request values to approve request
   // Make POST /thirdpartyTransactions/{transactionRequestId}/approve to config.shared.thirdpartySdkEndpoint
 
-  // POST /thirdpartyTransactions/{transactionRequestId}/approve
+  const thirdpartyTransactionIDApproveRequest: thirdpartySdk.components["schemas"]["ThirdpartyTransactionIDApproveRequest"] = {
+    authorizationResponse: {
+      responseType: 'ACCEPTED',
+      signedPayload: {
+        signedPayloadType: "FIDO",
+        fidoSignedPayload: {
+          id: "45c-TkfkjQovQeAWmOy-RLBHEJ_e4jYzQYgD8VdbkePgM5d98BaAadadNYrknxgH0jQEON8zBydLgh1EqoC9DA",
+          rawId: "45c+TkfkjQovQeAWmOy+RLBHEJ/e4jYzQYgD8VdbkePgM5d98BaAadadNYrknxgH0jQEON8zBydLgh1EqoC9DA==",
+          response: {
+            authenticatorData: "SZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2MBAAAACA==",
+            clientDataJSON:
+              "eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiQUFBQUFBQUFBQUFBQUFBQUFBRUNBdyIsIm9yaWdpbiI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDIxODEiLCJjcm9zc09yaWdpbiI6ZmFsc2UsIm90aGVyX2tleXNfY2FuX2JlX2FkZGVkX2hlcmUiOiJkbyBub3QgY29tcGFyZSBjbGllbnREYXRhSlNPTiBhZ2FpbnN0IGEgdGVtcGxhdGUuIFNlZSBodHRwczovL2dvby5nbC95YWJQZXgifQ==",
+            signature:
+              "MEUCIDcJRBu5aOLJVc/sPyECmYi23w8xF35n3RNhyUNVwQ2nAiEA+Lnd8dBn06OKkEgAq00BVbmH87ybQHfXlf1Y4RJqwQ8="
+          },
+          type: "public-key"
+        }
+      }
+    }
+  }
 
-  // {
-  //   authorizationResponse: {
-  //       responseType: 'ACCEPTED',
-  //       signedPayload: {
-  //         signedPayloadType: 'FIDO',
-  //         fidoSignedPayload: {
-  //           id: TransferFundsRequest.challengeResults[0].fidoAssertion.id,
-  //           rawId: TransferFundsRequest.challengeResults[0].fidoAssertion.rawId,
-  //           response: {
-  //             authenticatorData: TransferFundsRequest.challengeResults[0].fidoAssertion.response.authenticatorData,
-  //             clientDataJSON: TransferFundsRequest.challengeResults[0].fidoAssertion.response.clientDataJSON,
-  //             signature: TransferFundsRequest.challengeResults[0].fidoAssertion.response.signature,
-  //             // What is userHandle in FIDO? Ignore for demo.
-  //           },
-  //           type: 'public-key'
-  //         }
-  //       }
-  //     }
-  // };
+  console.log('thirdpartyTransactionIDApproveRequest:')
+  console.dir(thirdpartyTransactionIDApproveRequest);
 
-  // Response
-  // {
-  //   "transactionStatus": {,
-  //     "transactionRequestState": "ACCEPTED",
-  //     "transactionState": "COMPLETED",
-  //   },",
-  //   "currentState": "transactionStatusReceived",
-  // }
-  // Core connector takes approve response and responds to PISP's transferFunds request.
+  const transactionRequestId = transferFundsRequest.challengeResults?.challengeOptionId //'b51ec534-ee48-4575-b6a9-ead2955b8069' // TODO: How do we get this from the thirdpartyTransactionsWithTransactionRequestIdInitiateRequest?
+  const thirdpartyTransactionPartyLookupUrl = `${config.shared.thirdpartySdkEndpoint}/thirdpartyTransaction/${transactionRequestId}/approve`
+  const thirdpartyTransactionIDApproveResponse = await axios.post<thirdpartySdk.components["schemas"]["ThirdpartyTransactionIDApproveResponseSuccess"] | thirdpartySdk.components["schemas"]["ThirdpartyTransactionIDApproveResponseError"]>(
+    thirdpartyTransactionPartyLookupUrl,
+    thirdpartyTransactionIDApproveRequest,
+    { // config
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    }
+  )
 
-  // POST /v3/transferFundsRequest Response
-  // {
-  //   "responseHeader": {
-  //     "responseTimestamp": {
-  //       "epochMillis": Date.now()
-  //     }
-  //   },
-  //   "result": approveResponse.transactionRequestState == "ACCEPTED" && approveResponse.transactionState == "COMPLETED" ? {"success": {}} : {"creditStatusUnknown": {}},
-  //   "paymentIntegratorTransactionId": "approveResponse.transactionStatus.transactionRequestId"
-  // }
+  console.log('thirdpartyTransactionIDApproveResponse:')
+  console.dir(thirdpartyTransactionIDApproveResponse?.data);
 
-  return h.response('TO BE DONE').code(200)
+  if (thirdpartyTransactionIDApproveResponse.data?.currentState === 'errored'){
+    const errorInformation = thirdpartyTransactionIDApproveResponse.data as thirdpartySdk.components["schemas"]["ThirdpartyTransactionIDApproveResponseError"]
+    // @ts-expect-error: TODO fix types for missing definitions
+    const fspiopError = ErrorHandler.Factory.reformatFSPIOPError(errorInformation)
+    throw fspiopError;
+  }
+
+  const approveResponse = thirdpartyTransactionIDApproveResponse.data as thirdpartySdk.components["schemas"]["ThirdpartyTransactionIDApproveResponseSuccess"]
+
+  const transferFundsResponse: gsp.components["schemas"]["TransferFundsResponse"] = {
+    "responseHeader": {
+      "responseTimestamp": {
+        "epochMillis": Date.now().toString()
+      }
+    },
+    "result": {
+      "success": {} // TODO: Handle failure cases
+    },
+    "paymentIntegratorTransactionId": approveResponse.transactionStatus.transactionRequestId
+  }
+
+  return h.response(transferFundsResponse).code(200)
 }
 
 export default {
